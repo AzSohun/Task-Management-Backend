9a4d26cee9778d060fea653f0a8e489f3bc8dc93
Asaduzzaman Sohun <148855210+code-with-sohun@users.noreply.github.com>
2026-05-17 01:08:19 +0600

    Fixed: ITaskRepository Interface error has been fixed

--- TaskManagement.Application/Interfaces/ITaskRepository.cs
+++ TaskManagement.Application/Interfaces/ITaskRepository.cs
@@ -10,7 +10,7 @@ namespace TaskManagement.Application.Interfaces
 
         Task<IEnumerable<TaskItem>> GetAllAsync();
         Task<TaskItem?> GetByIdAsync(Guid id);
-        Task AddAsycn(TaskItem taskItem);
+        Task AddAsync(TaskItem taskItem);
         Task UpdateAsync(TaskItem taskItem);
         Task DeleteAsync(Guid id);
 
