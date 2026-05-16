ee202f713930d2cb52731d0e616f56d23763652c
Asaduzzaman Sohun <148855210+code-with-sohun@users.noreply.github.com>
2026-05-16 23:05:05 +0600

    Added Task Item

---
+++ TaskManagement.Domain/Entities/TaskItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskManagement.Domain.Entities
+{
+    internal class TaskItem
+    {
+        public Guid id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; }
+        public DateTime ? UpdatedAt { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+}
