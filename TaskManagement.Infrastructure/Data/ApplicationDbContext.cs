ac6b1f48d2905d6132ad260a40b31206ba6c72b5
Asaduzzaman Sohun <148855210+code-with-sohun@users.noreply.github.com>
2026-05-16 23:51:26 +0600

    Added: Entity Framework and ApplicationDbContext class have been Added

---
+++ TaskManagement.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TaskManagement.Domain.Entities;
+
+namespace TaskManagement.Infrastructure.Data
+{
+    internal class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+            
+        }
+
+        public DbSet<TaskItem> TaskItems { get; set; }
+
+    }
+}
