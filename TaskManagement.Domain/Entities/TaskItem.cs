c22db1cbb97859b532c97c390a0fd7663c140bfc
Asaduzzaman Sohun <148855210+code-with-sohun@users.noreply.github.com>
2026-05-17 01:04:48 +0600

    Added TaskRepository Class

--- TaskManagement.Domain/Entities/TaskItem.cs
+++ TaskManagement.Domain/Entities/TaskItem.cs
@@ -6,7 +6,7 @@ namespace TaskManagement.Domain.Entities
 {
     public class TaskItem
     {
-        public Guid id { get; set; }
+        public Guid Id { get; set; }
         public string Title { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
         public DateTime CreatedAt { get; set; }
---
+++ TaskManagement.Infrastructure/Repositories/TaskRepository.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskManagement.Application.Interfaces;
+using TaskManagement.Domain.Entities;
+using TaskManagement.Infrastructure.Data;
+
+namespace TaskManagement.Infrastructure.Repositories
+{
+    internal class TaskRepository: ITaskRepository
+    {
+
+        private readonly ApplicationDbContext _context;
+
+        public TaskRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+
+        public async Task<IEnumerable<TaskItem>> GetAllAsync()
+        {
+            return await _context.TaskItems.ToListAsync();
+        }
+
+        public async Task<TaskItem?> GetByIdAsync(Guid id)
+        {
+            return await _context.TaskItems.FindAsync(id);
+        }
+
+
+        public async Task AddAsync(TaskItem taskItem)
+        {
+            await _context.TaskItems.AddAsync(taskItem);
+            await _context.SaveChangesAsync();
+        }
+
+
+        public async Task UpdateAsync(TaskItem taskItem)
+        {
+            _context.TaskItems.Update(taskItem);
+            await _context.SaveChangesAsync();
+        }
+
+
+        public async Task DeleteAsync(Guid id)
+        {
+            var user = await _context.TaskItems.FindAsync(id);
+
+            if(user != null)
+            {
+                _context.TaskItems.Remove(user);
+                await _context.SaveChangesAsync();
+            }
+        }
+
+
+    }
+}
