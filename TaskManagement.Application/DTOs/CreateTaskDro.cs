4f0ee13cc795644a991412c8d5492d94d7b5a81f
Asaduzzaman Sohun <148855210+code-with-sohun@users.noreply.github.com>
2026-05-17 01:37:06 +0600

    Added Create Task Dto

---
+++ TaskManagement.Application/DTOs/CreateTaskDro.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskManagement.Application.DTOs
+{
+    internal class CreateTaskDro
+    {
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+    }
+}
