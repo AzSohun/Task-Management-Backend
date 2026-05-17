4392a08d61b2d4ac91a292ed1bae00ce1f3fbb26
Asaduzzaman Sohun <148855210+code-with-sohun@users.noreply.github.com>
2026-05-17 12:18:06 +0600

    Added: Create Task Validator Dto has been added

---
+++ TaskManagement.Application/Validators/CreateTaskDtoValidator.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TaskManagement.Application.DTOs;
+
+namespace TaskManagement.Application.Validators
+{
+    internal class CreateTaskDtoValidator: AbstractValidator<CreateTaskDro>
+    {
+
+        public CreateTaskDtoValidator()
+        {
+
+            RuleFor(x => x.Title)
+                .NotEmpty().WithMessage("Title is Required")
+                .MaximumLength(100).WithMessage("Title Must Not Exceed 100 Characters");
+
+            RuleFor(x => x.Description)
+                .MaximumLength(500).WithMessage("Title Must Not Exceed 100 Characters");
+
+        }
+
+    }
+}
