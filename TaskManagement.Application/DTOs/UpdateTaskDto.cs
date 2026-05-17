c2c29967dde5afa9008cf12d4dacfbeb09f97193
Asaduzzaman Sohun <148855210+code-with-sohun@users.noreply.github.com>
2026-05-17 21:27:08 +0600

    Added Update Task Dto

---
+++ TaskManagement.Application/DTOs/UpdateTaskDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskManagement.Application.DTOs
+{
+    public class UpdateTaskDto
+    {
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public bool IsCompleted { get; set; }
+    }
+}
